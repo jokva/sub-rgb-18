Add triangle color bar and result map in UI & App.py Fix normalisation performance

app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
-import transfer
+import rgblend
 
 
 app = Flask(__name__)
@@ -11,28 +11,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def index_page():
-    image1 = import_image('test_data/horizon1.nc', 'static/horizon1.png')
-    image2 = import_image('test_data/horizon2.nc', 'static/horizon2.png')
-    image3 = import_image('test_data/horizon3.nc', 'static/horizon3.png')
 
+    data1 = xr.open_dataarray('test_data/horizon1.nc').values
+    data2 = xr.open_dataarray('test_data/horizon2.nc').values
+    data3 = xr.open_dataarray('test_data/horizon3.nc').values
 
+    image1 = create_image(data1, 'static/horizon1.png')
+    image2 = create_image(data2, 'static/horizon2.png')
+    image3 = create_image(data3, 'static/horizon3.png')
 
-    # rgb = transfer.transfer()
+    prop = rgblend.normalize3arrays(data1.ravel(), data2.ravel(), data3.ravel())
+
+    xy, rgbd = rgblend.transfer(prop)
+
+    #rgbd = np.random.randn(data1.shape[0] * data1.shape[1], 3)
 
     # convert values to 0 - 255 int8 format
-    # im = Image.new("RGB", rgb.scale)
-    # im.putdata(rgb)
-    # im.save("static/result.png")
+    rgbi = (rgbd * 255).astype(int)
+    rgbi = [tuple(c) for c in rgbi]
 
-    imageresult = 'image2' #TODO
+    imageresult = 'static/result.png'
+    img = Image.new('RGB', data1.shape[::-1])
+    img.putdata(rgbi)
+    img.save(imageresult)
+
+    triangle = 'static/triangle.png'
+    fig = plt.figure()
+    plt.scatter(xy[:, 0], xy[:, 1], c=rgbd)
+    fig.savefig('static/triangle.png')
 
 
-    dictionary = {'image1': image1,'image2': image2,'image3': image3,'result': imageresult}
+    dictionary = {'image1': image1,'image2': image2,'image3': image3,'result': '/' + imageresult, 'triangle': '/' + triangle}
     return render_template('index.html', content=dictionary)
 
 
-def import_image(file, name):
-    data = xr.open_dataarray(file).values
+def create_image(data, name):
     dim = data.shape[::-1]
     f = data.flatten()
     array = ((f - np.min(f)) / (np.max(f) - np.min(f)) * 255).astype(int)

rgblend/normalize.py
@@ -15,14 +15,11 @@ def normalize3arrays(array1, array2, array3):
     narray3 = normalizeArray2Range(array3)
     return computeProportion3Arrays(narray1, narray2, narray3)
 
+
 # compute the normalized version of each array
 def normalizeArray2Range(array):
-    normalizedArray = np.zeros(len(array))
-    for j in range(0, len(array)):
-        if (max(array) - min(array)) == 0:
-            continue
-        normalizedArray[j] = (array[j] - min(array)) / (max(array) - min(array))
-    return normalizedArray
+    return (array - np.min(array)) / (np.max(array) - np.min(array))
+
 
 def computeProportion3Arrays(array1, array2, array3):
 
@@ -37,9 +34,9 @@ def computeProportion3Arrays(array1, array2, array3):
             globallyNormalized1stTable[i] = 1 / 3.0
             globallyNormalized2ndTable[i] = 1 / 3.0
             globallyNormalized3rdTable[i] = 1 / 3.0
-            continue
-        globallyNormalized1stTable[i] = array1[i] / norm
-        globallyNormalized2ndTable[i] = array2[i] / norm
-        globallyNormalized3rdTable[i] = array3[i] / norm
+        else:
+            globallyNormalized1stTable[i] = array1[i] / norm
+            globallyNormalized2ndTable[i] = array2[i] / norm
+            globallyNormalized3rdTable[i] = array3[i] / norm
 
     return globallyNormalized1stTable, globallyNormalized2ndTable, globallyNormalized3rdTable