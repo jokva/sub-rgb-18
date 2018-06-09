2cb5419fadfa4605ca6b9493d66854fa58a3d356
Maxmilian Schuberth <xschuberth@gmail.com>
2018-06-09 13:28:45 +0200

    rename package to rgblend and add content

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ import setuptools
 
 setuptools.setup(
     name = 'sub-rgb',
+    packages = ['rgblend'],
     install_requires = ['matplotlib', 'numpy'],
     setup_requires = ['setuptools>=28', 'pytest-runner'],
     tests_require = ['pytest'],
+
 )
