6a85950fd98918f0bb9824fcdfa8f71d3063e992
Maxmilian Schuberth <xschuberth@gmail.com>
2018-06-10 12:54:50 +0200

    change access to plotting maps

--- rgblend/__init__.py
+++ rgblend/__init__.py
@@ -2,5 +2,5 @@ from .normalize import normalize3arrays, normalize3arrays_numpy
 from .transfer import transfer, xy2rgbd
 from .importfiles import import3images
 from .triangles import triangles
-from .plotting import tribar
+from .plotting import tribar, rgblend
 
