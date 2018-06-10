b0277e5cc9a5ee24c5778829455d882084a65884
Maxmilian Schuberth <xschuberth@gmail.com>
2018-06-10 09:04:13 +0200

    fix __init__ by importing triangles not as part of transfer.. sorry

--- rgblend/__init__.py
+++ rgblend/__init__.py
@@ -1,4 +1,5 @@
 from .normalize import normalize3arrays
-from .transfer import transfer, xy2rgbd, triangles
+from .transfer import transfer, xy2rgbd
 from .importfiles import import3images
+from .triangles import triangles
 
