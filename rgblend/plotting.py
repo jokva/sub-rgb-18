f7484290a5565520043b6ac99ae209c7bbfcf13e
Per Olav Svendsen <perolav@outlook.com>
2018-06-10 10:13:58 +0200

    Added labels to tribar plot

--- rgblend/plotting.py
+++ rgblend/plotting.py
@@ -6,10 +6,9 @@ import numpy as np
 
 
 
-def tribar(figsize = [5,5], xl = 1, d =10):
+def tribar(figsize = [5,5], xl = 1, d = 10, labels = ['Red', 'Green', 'Blue']):
     """
     Return figure object that contains a vectorized triangle
-
     :param figsize:
     :param xl: length of triangle side
     :param d: number of discretizations along triangle side
@@ -26,16 +25,27 @@ def tribar(figsize = [5,5], xl = 1, d =10):
     # Edge of triangles
     edge = xl * np.array([[-0.5, 0], [0.5, 0], [0, np.sqrt(.75)], [-0.5, 0]])
 
-    rgba = np.vstack([rgblend.xy2rgbd(xyc, xl).T, np.ones(len(xyc))]).T
+    rgba = np.vstack([rgblend.xy2rgbd(xyc, 1).T, np.ones(len(xyc))]).T
     pc.set_facecolors(rgba)
 
     ax.add_collection(pc)
+    
+    # Hide native yaxis
+    plt.yticks([])
+    
+    # Add labels to corners
+    corners = [(-0.5, 0.0), (0.0, np.sqrt(.75)), (0.5, 0.0)]
+    valigns = ['top', 'bottom', 'top']
+    haligns = ['right', 'center', 'left'] 
+    for corner, va, ha, label in zip(corners, valigns, haligns, labels):
+        x, y = corner
+        plt.text(x, y, label, ha=ha, va=va, fontsize=15)
 
     plt.plot(edge[:, 0], edge[:, 1], 'k-', lw=0.5)
     for loc in ['top', 'bottom', 'left', 'right']:
         ax.spines[loc].set_visible(False)
-    plt.xlim([-0.5*xl, .5*xl])
-    plt.ylim([0, xl*np.sqrt(.75)])
+    plt.xlim([-0.5, .5])
+    plt.ylim([0, np.sqrt(.75)])
     plt.gca().set_aspect('equal')
 
     return fig
