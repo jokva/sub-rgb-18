de544d0c1cd74771e3b03e7828ed998f6bfa984d
Jørgen Kvalsvik <jokva@statoil.com>
2018-06-10 10:53:13 +0200

    Refactor triangles

--- rgblend/triangles.py
+++ rgblend/triangles.py
@@ -1,54 +1,135 @@
 import numpy as np
 
+def coordinates(row, outer_width, resolution):
+    """
+    The coordintes at which there must be a an inner triangle (bottom) corner
+    for a given row
 
-def _triangle_row(r, xl, d):
+    Parameters
+    ----------
+    row : int
+    outer_width : int
+    resolution : int
+
+    Returns
+    -------
+    coords : numpy.ndarray
+    """
+    return np.linspace(0.5 * outer_width * (-1 + row / resolution),
+                       0.5 * outer_width * ( 1 - row / resolution),
+                       resolution + 1 - row)
+
+def upwards(row, outer_width, height, resolution):
+    """
+    Build a list of coordinate-triplets (i,j,k) for one row of triangles,
+    facing upwards::
+
+           k
+          / \
+         /   \
+        i-----j
+
+    Parameters
+    ----------
+    row : int
+        the row to build for, where 0 is the bottom row
+    outer_width : int
+        the length of the outer triangle's sides
+    height : int
+        the height of each inner triangle (dictates scale of resulting figure)
+    resolution : int
+        resolution, or discretisation, the number of triangles along each side
+        of the (full) triangle
+
+    Returns
+    -------
+    coords : iterable of [i], [j], [k] lists, each of (x,y) coordinate lists
+    """
+
+    bottom = np.full(shape = resolution, fill_value = height * row)
+    top    = np.full(shape = resolution, fill_value = height * (row + 1))
+
+    # the points of triangle corners along the row's bottom line
+    coords = coordinates(row, outer_width, resolution)
+
+    left = zip(coords[:-1], bottom)
+    right = zip(coords[1:], bottom)
+    upper = zip((coords[1:] + coords[:-1]) / 2, top)
+
+    return zip(left, right, upper)
+
+def downwards(row, outer_width, height, resolution):
+    """
+    Build a list of coordinate-triplets (i,j,k) for one row of triangles,
+    facing downwards::
+
+        i-----j
+         \   /
+          \ /
+           k
+
+    Parameters
+    ----------
+    row : int
+        the row to build for, where 0 is the bottom row
+    outer_width : int
+        the length of the outer triangle's sides
+    height : int
+        the height of each inner triangle (dictates scale of resulting figure)
+    resolution : int
+        resolution, or discretisation, the number of triangles along each side
+        of the (full) triangle
+
+    Returns
+    -------
+    coords : iterable of [i], [j], [k] lists, each of (x,y) coordinate lists
+    """
+
+    # downwards facing triangles for this row share line with upwards facing
+    # coordinates of the next row (up), so this problem reduces to replacing
+    # the next row's triangles top coordinate with a lower one
+    xs = upwards(row + 1, outer_width, height, resolution)
+
+    def f(x):
+        left, right, up = x
+        up = (up[0], height * row)
+        return left, right, up
+
+    return map(f, xs)
+
+def centers(row, outer_width, height, resolution):
+    coords = coordinates(row, outer_width, resolution)
+    middles = (coords[1:] + coords[:-1]) / 2
+
+    ups = np.vstack([
+        middles,
+        np.full(len(coords) - 1, height * row + height / 3)
+    ]).T
+
+    coords = coordinates(row + 1, outer_width, resolution)
+    middles = (coords[1:] + coords[:-1]) / 2
+
+    downs = np.vstack([
+        (coords[1:] + coords[:-1]) / 2,
+        np.full(len(coords) - 1, height * (row + 1) + 2 * height / 3 - height)
+    ]).T
+
+    return np.vstack([ups, downs])
+
+def triangle_row(r, xl, d):
+
+    resolution = d
 
     # Height of triangles
-    ht = xl * np.sqrt(.75) / d
+    ht = xl * np.sqrt(.75) / resolution
 
-    # Bottom x coordinates
-    bd = np.linspace(.5 * xl * (-1 + r / d), .5 * xl * (1 - r / d), d + 1 - r)
+    ups = [np.asarray(x) for x in upwards(r, xl, ht, resolution)]
+    downs = [np.asarray(x) for x in downwards(r, xl, ht, resolution)]
+    cents = centers(r, xl, ht, resolution)
 
-    # Upwards facing
-    # lower left
-    cu0 = list(zip(bd[:-1], np.ones(d) * ht * r))
-    # lower right
-    cu1 = list(zip(bd[1:], np.ones(d) * ht * r))
-    # upper
-    cu2 = list(zip((bd[1:] + bd[:-1]) / 2, np.ones(d) * ht * (r + 1)))
+    return ups + downs, cents
 
-    # Collect upwards facing triangles in list of arrays with each of shape (3,2)
-    tru = list(map(np.asarray, list(zip(cu0, cu1, cu2))))
-
-    # Collect center coordinates
-    xuc = (bd[1:] + bd[:-1]) / 2
-    yuc = np.ones(len(xuc)) * (ht * r + ht / 3)
-    xyuc = np.vstack([xuc, yuc]).T
-
-    # Downwards facing
-    r += 1
-    # Upper x coordinates of downwards facing
-    bd = np.linspace(.5 * xl * (-1 + r / d), .5 * xl * (1 - r / d), d + 1 - r)
-    # upper left
-    cd0 = list(zip(bd[:-1], np.ones(d) * ht * r))
-    # upper right
-    cd1 = list(zip(bd[1:], np.ones(d) * ht * r))
-    # lower
-    cd2 = list(zip((bd[1:] + bd[:-1]) / 2, np.ones(d) * ht * (r - 1)))
-
-    trd = list(map(np.asarray, list(zip(cd0, cd1, cd2))))
-
-    xdc = (bd[1:] + bd[:-1]) / 2
-    ydc = np.ones(len(xdc)) * (ht * (r) + 2 * ht / 3 - ht)
-    xydc = np.vstack([xdc, ydc]).T
-
-    # Collect upwards and downwards facing centers
-    xyc = np.vstack([xyuc, xydc])
-
-    tr = tru + trd
-    return tr, xyc
-
-def triangles(xl, d):
+def triangles(length, resolution):
     """
     Compute triangle coordinates
 
@@ -60,8 +141,8 @@ def triangles(xl, d):
 
     tris, xyc = [], []
 
-    for r in range(d):
-        trir, xycr =  _triangle_row(r, xl, d)
+    for r in range(resolution):
+        trir, xycr = triangle_row(r, length, resolution)
         tris.append(trir)
         xyc.append(xycr)
     tris = sum(tris,[])
